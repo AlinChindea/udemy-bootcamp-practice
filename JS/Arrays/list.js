d113a951f5221451bd3b07bb22f37186d8a04d3e
Alin Chindea <alin.chindea@gmail.com>
2018-03-29 14:20:41 +0100

    introduced forEach, splice to remove item from list, refactored code

--- JS/Arrays/list.js
+++ JS/Arrays/list.js
@@ -4,15 +4,38 @@ window.setTimeout(function() {
   while(input !== 'quit') {
     //handle input
     if(input === 'list') {
-      console.log(todos);
+      listTodos();
     } else if(input === 'new') {
-      //ask for new todo
-      const newTodo = prompt('Enter new todo');
-      //add to todos array
-      todos.push(newTodo);
+      addTodo();
+    } else if(input === 'delete') {
+      deleteTodo();
     }
     //ask again for new input
     input = prompt('What would you like to do?');
   }
   console.log('ok, you quit the app');
 }, 500);
+
+function listTodos() {
+  console.log('**********');
+  todos.forEach(function(todo, index){
+    console.log(index + ': ' + todo);
+  });
+  console.log('**********');
+}
+
+function addTodo() {
+  //ask for new todo
+  const newTodo = prompt('Enter new todo');
+  //add to todos array
+  todos.push(newTodo);
+  console.log('Added todo');
+}
+
+function deleteTodo() {
+  //ask for index of todo to be deleted
+  const index = prompt('Enter index of todo to delete');
+  //delete that todo using splice()
+  todos.splice(index,1);
+  console.log('Deleted Todo');
+}
